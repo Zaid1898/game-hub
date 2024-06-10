0f6159b3387fdbc45285024a50bbd6f4f4c1b2ef
Zaid <zaid568124@gmail.com>
2024-06-10 18:17:03 +0500

    Refactor : Extract a Query Object

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,17 +1,16 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames, { Platform } from "../hooks/useGames";
+import useGames from "../hooks/useGames";
 import GameCards from "./GameCards";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { Genre } from "../hooks/useGenre";
+import { GameQuery } from "../App";
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatfom : Platform | null;
+  gameQuery : GameQuery
 }
 
-const GameGrid = ({ selectedGenre , selectedPlatfom }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre , selectedPlatfom);
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   
   return (
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,5 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenre";
 
 export interface Platform {
   id: number;
@@ -15,10 +15,14 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (selectedGenre: Genre | null , selectedPlatform : Platform | null) => {
-  console.log("Fetching games for genre ID:", selectedGenre?.id);
-  return useData<Game>('/games', { params: { genres: selectedGenre?.id  , platforms : selectedPlatform?.id}},
-    [selectedGenre?.id, selectedPlatform?.id]
+const useGames = (gameQuery : GameQuery) => {
+  return useData<Game>('/games', 
+    { params: 
+      { 
+        genres: gameQuery.genre?.id  ,
+        platforms : gameQuery.platform?.id
+      }},
+    [gameQuery]
 
   );
 };
