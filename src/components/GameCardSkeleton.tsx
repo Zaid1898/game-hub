65036e2a426445616602988919dc45fc549b7e6d
Zaid <zaid568124@gmail.com>
2024-06-10 12:16:00 +0500

    Adding Spinner in Genre List

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -1,10 +1,11 @@
-import { Card,CardBody, SkeletonText } from '@chakra-ui/react'
+import { Card,CardBody,Skeleton, SkeletonText } from '@chakra-ui/react'
 
 
 
 const GameCardSkeleton = () => {
   return (
     <Card>
+        <Skeleton height="200px" />
         <CardBody>
             <SkeletonText/>
         </CardBody>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,24 @@
+import { HStack, Image, List, ListItem,Spinner,Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenre";
+import getCroppedImageURL from "../services/image-url";
+
 
 const GenreList = () => {
-    const {data} = useGenres(); 
+    const {data, isLoading , error} = useGenres(); 
+    if (error) return null;
+    if (isLoading) return <Spinner/>
   return (
-    <ul >
-        {data.map(genre => <li key ={genre.id}>{genre.name}</li>)}
-    </ul>
+    <List >
+        {data.map(genre => 
+        <ListItem key ={genre.id} paddingY="4px" >
+          <HStack>
+            <Image  boxSize={"32px"} borderRadius={"8px"} src = {getCroppedImageURL(genre.image_background)}/>
+            <Text>
+              {genre.name}
+            </Text>
+          </HStack> 
+        </ListItem>)}
+    </List>
   )
 }
 
